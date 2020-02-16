9286d4f12a73bf8662b3f5e96c483732a456c720
Yuhn-z <1029363172@qq.com>
2020-02-16 18:11:07 +0800

    add check...

--- cmd/check.go
+++ cmd/check.go
@@ -17,9 +17,38 @@ func checkFunc(cmd *cobra.Command, args []string) {
 
 	//如果开启了debug
 	if debug {
-
 		cmd.Printf("Parsed configuration: CacheTTL: %s, Port: %d\n", v.CacheTTL.String(), v.Port)
-		// TODO
+		cmd.Printf("Hosts: %v\n", v.Host)
+
+		cmd.Printf("Extra (%d):\n", len(v.ExtraConfig))
+		for k, e := range v.ExtraConfig {
+			cmd.Printf("  %s: %v\n", k, e)
+		}
+
+		cmd.Printf("Endpoints (%d):\n", len(v.Endpoints))
+		for _, endpoint := range v.Endpoints {
+			cmd.Printf("\tEndpoint: %s, Method: %s, CacheTTL: %s, Concurrent: %d, QueryString: %v\n",
+				endpoint.Endpoint, endpoint.Method, endpoint.CacheTTL.String(),
+				endpoint.ConcurrentCalls, endpoint.QueryString)
+
+			cmd.Printf("\tExtra (%d):\n", len(endpoint.ExtraConfig))
+			for k, e := range endpoint.ExtraConfig {
+				cmd.Printf("\t  %s: %v\n", k, e)
+			}
+
+			cmd.Printf("\tBackends (%d):\n", len(endpoint.Backends))
+			for _, backend := range endpoint.Backends {
+				cmd.Printf("\t\tURL: %s, Method: %s\n", backend.URLPattern, backend.Method)
+				cmd.Printf("\t\t\tTimeout: %s, Target: %s, Mapping: %v, BL: %v, WL: %v, Group: %v\n",
+					backend.Timeout, backend.Target, backend.Mapping, backend.Blacklist, backend.Whitelist,
+					backend.Group)
+				cmd.Printf("\t\t\tHosts: %v\n", backend.Host)
+				cmd.Printf("\t\t\tExtra (%d):\n", len(backend.ExtraConfig))
+				for k, e := range backend.ExtraConfig {
+					cmd.Printf("\t\t\t  %s: %v\n", k, e)
+				}
+			}
+		}
 
 	}
 	//如果错误
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.13
 require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/clbanning/mxj v1.8.4
+	github.com/gin-gonic/gin v1.5.0
 	github.com/mmcdole/gofeed v1.0.0-beta2
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/pelletier/go-toml v1.6.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v0.0.5
