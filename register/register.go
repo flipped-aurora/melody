8a9ada8feace3288ee58a90322cd2e81bf380cc3
Yuhn-z <1029363172@qq.com>
2020-02-24 03:01:39 +0800

    add melody rigister and test

--- register/register.go
+++ register/register.go
@@ -11,3 +11,38 @@ type Untyped interface {
 func New() Untyped {
 	return internal.New()
 }
+
+func NewSpaces() *Namespaced {
+	return &Namespaced{New()}
+}
+
+type Namespaced struct {
+	data Untyped
+}
+
+func (n *Namespaced) Get(namespace string) (Untyped, bool) {
+	v, ok := n.data.Get(namespace)
+	if !ok {
+		return nil, ok
+	}
+	register, ok := v.(Untyped)
+	return register, ok
+}
+
+func (n *Namespaced) Register(namespace, name string, v interface{}) {
+	if register, ok := n.Get(namespace); ok {
+		register.Register(name, v)
+		return
+	}
+
+	register := New()
+	register.Register(name, v)
+	n.data.Register(namespace, register)
+}
+
+func (n *Namespaced) AddNamespace(namespace string) {
+	if _, ok := n.Get(namespace); ok {
+		return
+	}
+	n.data.Register(namespace, New())
+}
