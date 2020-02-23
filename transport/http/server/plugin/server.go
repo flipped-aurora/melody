955a0cde0401c48a4c0722e890fbeb788c7e6fc8
Yuhn-z <1029363172@qq.com>
2020-02-24 02:11:35 +0800

    update func new() in transport\http\server\plugin

--- transport/http/server/plugin/server.go
+++ transport/http/server/plugin/server.go
@@ -16,7 +16,19 @@ type RunServer func(context.Context, config.ServiceConfig, http.Handler) error
 // New 返回下一个RunServer
 func New(logger logging.Logger, next RunServer) RunServer {
 	return func(ctx context.Context, cfg config.ServiceConfig, handler http.Handler) error {
-		//TODO 根据配置文件检察是否开启handler
+		// 根据配置文件检察是否开启handler
+		v, ok := cfg.ExtraConfig[Namespace]
+		if !ok {
+			logger.Debug("melody_http_server_handler: no extra config")
+			return next(ctx, cfg, handler)
+		}
+		extra, ok := v.(map[string]interface{})
+		if !ok {
+			logger.Debug("melody_http_server_handler: wrong extra config type")
+			return next(ctx, cfg, handler)
+		}
+
+		// TODO load plugin(s)
 		return next(ctx, cfg, handler)
 	}
 }
