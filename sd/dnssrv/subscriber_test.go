16c48c51840c5e36e476ace05937b9ea6bf04bd1
wangzhen <1046060018@qq.com>
2020-02-25 11:23:00 +0800

    add subscriber_test.go

---
+++ sd/dnssrv/subscriber_test.go
@@ -0,0 +1,61 @@
+package dnssrv
+
+import (
+	"errors"
+	"melody/config"
+	"melody/sd"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestSubscriber_New(t *testing.T) {
+	if err := Register(); err != nil {
+		t.Error("registering the dns module:", err.Error())
+	}
+	srvSet := []*net.SRV{
+		{
+			Port:   80,
+			Target: "127.0.0.1",
+		},
+		{
+			Port:   81,
+			Target: "127.0.0.1",
+		},
+	}
+	DefaultLookup = func(service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+		return "cname", srvSet, nil
+	}
+
+	s := sd.GetSubscriber(&config.Backend{Host: []string{"www.baidu.com"}, SD: Namespace})
+	hosts, err := s.Hosts()
+
+	if err != nil {
+		t.Error("Getting the hosts:", err.Error())
+	}
+	if len(hosts) != 2 {
+		t.Error("Wrong number of hosts:", len(hosts))
+	}
+	if hosts[0] != "http://127.0.0.1:80" {
+		t.Error("Wrong host #0 (expected http://127.0.0.1:80):", hosts[0])
+	}
+	if hosts[1] != "http://127.0.0.1:81" {
+		t.Error("Wrong host #1 (expected http://127.0.0.1:81):", hosts[1])
+	}
+}
+
+func TestSubscriber_LoockupError(t *testing.T) {
+	errToReturn := errors.New("Some random error")
+	defaultLookup := func(service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+		return "cname", []*net.SRV{}, errToReturn
+	}
+	ttl := 1 * time.Millisecond
+	s := NewDetailed("some.example.tld", defaultLookup, ttl)
+	hosts, err := s.Hosts()
+	if err != nil {
+		t.Error("Unexpected error!", err)
+	}
+	if len(hosts) != 0 {
+		t.Error("Wrong number of hosts:", len(hosts))
+	}
+}
