b87d82249a69c1701f0948aca59c5e88810b677b
Granty1 <granty1@163.com>
2020-05-06 20:04:58 +0800

    Remove debug info in debug handlerfunc

--- router/gin/debug.go
+++ router/gin/debug.go
@@ -1,8 +1,6 @@
 package gin
 
 import (
-	"io/ioutil"
-
 	"github.com/gin-gonic/gin"
 
 	"melody/logging"
@@ -11,14 +9,6 @@ import (
 // DebugHandler creates a dummy handler function, useful for quick integration tests
 func DebugHandler(logger logging.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger.Debug("Method:", c.Request.Method)
-		logger.Debug("URL:", c.Request.RequestURI)
-		logger.Debug("Query:", c.Request.URL.Query())
-		logger.Debug("Params:", c.Params)
-		logger.Debug("Headers:", c.Request.Header)
-		body, _ := ioutil.ReadAll(c.Request.Body)
-		c.Request.Body.Close()
-		logger.Debug("Body:", string(body))
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
